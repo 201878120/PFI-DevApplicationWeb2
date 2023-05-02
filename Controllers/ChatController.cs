3f2b7ed2ba6cfdcb16c5a0070bb9cf8dbfee0db2
201878120 <smurfowowatch@gmail.com>
2023-05-02 17:42:41 -0400

    initial implementation of chat controller & friends fetcher

--- Controllers/ChatController.cs
+++ Controllers/ChatController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ChatManager.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +13,82 @@ namespace ChatManager.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public ContentResult GetFriendsList()
+        {
+            string s = "";
+            List<Dictionary<string, string>> amis = new List<Dictionary<string, string>>();
+            Dictionary<string, string> c = new Dictionary<string, string>
+            {
+                { "Name", "Saliha Yacoub" },
+                { "UserId", "2" },
+                { "Image", "4491ed49-2848-4618-b131-7036cb07126f" }
+            };
+            amis.Add(c);
+            Dictionary<string, string> c2 = new Dictionary<string, string>
+            {
+                { "Name", "Stéphane Chassé" },
+                { "UserId", "3" },
+                { "Image", "89ca9b58-9e3d-4c21-bb08-369f8f04b2fb" }
+            };
+            amis.Add(c2);
+
+            foreach(Dictionary<string, string> ami in amis)
+            {
+                s += 
+                "<div class=\"unselectedTarget\" userid=\"" + ami["UserId"] + "\">\r\n" +
+                    "<div class=\"UserSmallAvatar\" title=\"" + ami["Name"] + "\" \r\n" +
+                        "style=\"background: url(/Images_Data/User_Avatars/" + ami["Image"] + ".Jpeg) \" >" +
+                    "</div>\r\n" +
+                "</div>\r\n";
+            }
+            return Content(s, "text/html");
+        }
+
+        [HttpGet]
+        public ContentResult GetMessages()
+        {
+            User currentUser = DB.Users.FindUser((int)Session["currentLoginId"]);
+            User otherUser = DB.Users.FindUser((int)Session["currentChattedId"]);
+            string s = 
+            "<div class='messagesHeader'>" +
+                "<h4>Conversation avec</h4>" +
+                "<div class='userItem'>" + 
+                    "<div class='UserMediumAvatar'" + 
+                        "style='background: url(/Images_Data/User_Avatars/" + otherUser.Avatar + ".Jpeg)'>" +
+                    "</div>" +
+                    "<div class='ellipsis'>" + otherUser.FirstName + otherUser.LastName + "</div>" + 
+                "</div>" + 
+            "</div>";
+            //Session["currentLoginId"]
+            //Session["currentChattedId"]
+            return Content(s, "text/html");
+        }
+
+        [HttpGet]
+        public void SetCurrentTarget(int id)
+        {
+            Session["currentChattedId"] = id;
+        }
+
+        [HttpGet]
+        public JsonResult Send(string message)
+        {
+            return Json("fake", JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public JsonResult Update(int id, string message)
+        {
+            return Json("fake", JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public JsonResult Delete(int id)
+        {
+            return Json("fake", JsonRequestBehavior.AllowGet);
         }
     }
 }
---
+++ Models/Message.cs
@@ -0,0 +1,21 @@
+using FileKeyReference;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace ChatManager.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public int FromUserId { get; set; }
+        public int ToUserId { get; set; }
+
+        [Display(Name = "Date de création")]
+        [DataType(DataType.Date)]
+        public DateTime WrittenDate { get; set; }
+        public string Content { get; set; }
+    }
+}
