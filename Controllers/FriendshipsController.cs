e9a2f2f8d29535ae02aebeef01bda2a94a93f9ca
JohnP55 <62863085+JohnP55@users.noreply.github.com>
2023-05-11 10:14:10 -0400

    progress on friendship

--- Controllers/FriendshipsController.cs
+++ Controllers/FriendshipsController.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using ChatManager.Models;
 using System.Web.Mvc;
 
 namespace ChatManager.Controllers
@@ -12,6 +9,19 @@ namespace ChatManager.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public JsonResult AreFriends(int userId1, int userId2)
+        {
+            return Json(DB.Friendships.AreFriends(userId1, userId2));
+        }
+
+        public JsonResult SendFriendRequest(int other)
+        {
+            User currentUser = OnlineUsers.GetSessionUser();
+            FriendshipStatus status = DB.Friendships.GetUsersStatus(currentUser.Id, other);
+            return Json(0, JsonRequestBehavior.AllowGet);
         }
     }
 }
